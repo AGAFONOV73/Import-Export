9f7350303fefb8cbd9cbf4648cc1c5c12d252cc7
ADL13 <a.d.l.13.02.73@gmail.com>
2025-10-09 00:00:49 +0500

    divide into modules

---
+++ game.js/game.js
@@ -0,0 +1,9 @@
+import Character from './domain.js';
+
+class Game { /* ... */ }
+
+function readGameSaving() { /* ... */ }
+function writeGameSaving() { /* ... */ }
+
+export default Game;
+export { GameSavingData, readGameSaving, writeGameSaving };
